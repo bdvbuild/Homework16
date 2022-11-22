db2d4c67d7f5d87dfe099a1afb0a31c85eef89cf
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-22 17:40:32 +0400

    Добавьте файлы проекта.

---
+++ Homework16/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+
+//1.Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json.
+//Разработать класс для моделирования объекта «Товар». Предусмотреть члены класса «Код товара» (целое число),
+//«Название товара» (строка), «Цена товара» (вещественное число).
+//Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+//Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Product[] products = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите код товара: ");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите название товара: ");
+                string name = Console.ReadLine();
+                Console.Write("Введите цену товара: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                products[i] = new Product { Code = code, Name = name, Price = price };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products, options);
+
+            using (StreamWriter sw = new StreamWriter("../../../Products.json")) 
+            {
+                sw.WriteLine(jsonString);
+            }
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+//2.Необходимо разработать программу для получения информации о товаре из json-файла.
+//Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product mostExp = products[0];
+            foreach (Product p in products)
+            {
+                if (p.Price > mostExp.Price)
+                    mostExp = p;
+            }
+            Console.WriteLine($"Самый дорогой товар: {mostExp.Name}\nцена: {mostExp.Price}");
+            Console.ReadKey();
+        }
+    }
+}
